8d1b2a24c5e81a198e009eb03fcabf296d7c8856
GitHub-Sanjit <sanjit.est14@gamil.com>
2024-05-03 23:51:57 +0600

    Customizing Map Popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,10 +1,19 @@
+// const campground = require("../../models/campground");
+
 mapboxgl.accessToken = mapToken;
 // ("pk.eyJ1Ijoic2Fuaml0bWFwYm94IiwiYSI6ImNsMjFqbHhmbzA1dTUzYm8xeTBwcTlrbnMifQ.60o_vri1H53dE81kflAj5w");
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: campground.geometry.coordinates,
   zoom: 9, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
